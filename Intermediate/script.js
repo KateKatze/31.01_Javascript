7ce8f7ea630dce2fd38afdce899c8ee5ce33465d
KateKatze <dr.katekogler@gmail.com>
2022-01-31 10:48:03 +0100

    ex int

---
+++ Intermediate/script.js
@@ -0,0 +1,37 @@
+// ex 1 | Compare two numbers
+
+// Create a JavaScript program that will show the average grade for each student. Here are the points for each student:
+// Martin = 76
+// Thomas = 85
+// Klaus = 65
+// Maria = 93
+// David = 81
+// Based on their points, output the grade, according to the following table:
+// < 60 F
+// < 70 D
+// < 80 C
+// < 90 B
+// < 100 A
+// Finally, calculate the class average and output it (e.g. if the class average is 74 points, their average grade will be C).
+
+var Mart = 76;
+var Thom = 85;
+var Klaus = 65;
+var Mar = 93;
+var Dav = 81;
+
+if ( < 60){
+    console.log( + "F");
+}
+if ( < 70){
+    console.log( + "D");
+}
+if ( < 80){
+    console.log( + "C");
+}
+if ( < 90){
+    console.log( + "B");
+}
+if ( < 100){
+    console.log( + "A");
+}
