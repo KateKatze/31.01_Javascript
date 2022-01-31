d9eed56da83269b952dc4172449781fccd9f1e6b
KateKatze <dr.katekogler@gmail.com>
2022-01-31 14:58:10 +0100

    ex 3 done

---
+++ Intermediate/ex.2/script.js
@@ -0,0 +1,11 @@
+for (var i = 0; i <= 100; i++){
+    if (i%3 === 0 && i%5 === 0){
+        console.log(i + "Fizz Buzz");
+    }else if(i%3 === 0){
+        console.log(i + "Fizz");
+    }else if (i%5 === 0){
+        console.log(i + "Buzz");
+    }else{
+        console.log(i);
+    }
+}
