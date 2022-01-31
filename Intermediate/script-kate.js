c8fcef68eb5ddbac57d5fe0409dd32d9df1bd45f
KateKatze <dr.katekogler@gmail.com>
2022-01-31 13:40:07 +0100

    switch made

--- Intermediate/script-kate.js
+++ Intermediate/script-kate.js
@@ -14,42 +14,70 @@
 // < 100 A
 // Finally, calculate the class average and output it (e.g. if the class average is 74 points, their average grade will be C).
 
-// var Mart = 76;
-// var Thom = 85;
-// var Klaus = 65;
-// var Mar = 93;
-// var Dav = 81;
-
-
 var arr = [76, 85, 65, 93, 81];
-
 var start = 0;
 for (var i in arr) {
   start += arr[i];
 }
 
-console.log(start / 5);
+var averSum = start / arr.length;
+console.log(averSum);
 
+switch (true){
+  case (averSum >= 90):
+  document.getElementById('string').innerHTML = "It´s an A! <br>"; 
+  break;
+  case (averSum<=89&&averSum>=80):
+  document.getElementById('string').innerHTML = "It´s a B! <br>";
+  break;
+  case (averSum<=79&&averSum>=70):
+  document.getElementById('string').innerHTML = "It´s a C! <br>";
+  break;
+  case (averSum<=69&&averSum>=60):
+  document.getElementById('string').innerHTML = "It´s a D! <br>";
+  break;
+  case (averSum<60):
+  document.getElementById('string').innerHTML = "It´s an F! <br>";
+  break;
+  }
 
 for (let i=0; i < arr.length; i++){
-if(arr [i] >= 90) {
-document.write("It´s an A! <br>");
-}
-if(arr [i]<=89&&arr [i]>=80) {
-document.write("It´s a B! <br>");
-}
-if(arr [i]<=79&&arr [i]>=70) {
-document.write("It´s a C! <br>");
-}
-if(arr [i]<=69&&arr [i]>=60) {
-document.write("It´s a D! <br>");
-}
-if(arr [i]<60) {
-document.write("It´s an F :( <br>");
-}
+  switch (true) {
+    case (arr[i] >= 90):
+    document.write("It´s an A! <br>"); 
+    break;
+    case (arr [i]<=89&&arr [i]>=80):
+    document.write("It´s a B! <br>");
+    break;
+    case (arr [i]<=79&&arr [i]>=70):
+    document.write("It´s a C! <br>");
+    break;
+    case (arr [i]<=69&&arr [i]>=60):
+    document.write("It´s a D! <br>");
+    break;
+    case (arr [i]<60):
+    document.write("It´s an F :( <br>");
+    break;
+  }
 }
 
+// Second try:
 
-
-
-
+// var arr = [76, 85, 65, 93, 81];
+// for (let i=0; i < arr.length; i++){
+// if(arr [i] >= 90) {
+// document.write("It´s an A! <br>");
+// }
+// if(arr [i]<=89&&arr [i]>=80) {
+// document.write("It´s a B! <br>");
+// }
+// if(arr [i]<=79&&arr [i]>=70) {
+// document.write("It´s a C! <br>");
+// }
+// if(arr [i]<=69&&arr [i]>=60) {
+// document.write("It´s a D! <br>");
+// }
+// if(arr [i]<60) {
+// document.write("It´s an F :( <br>");
+// }
+// }
